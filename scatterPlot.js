7e5481e15bd9b83e4b6c66cb642ce4e505dbba83
Vladyslav <kytsevolov@gmail.com>
2021-02-08 17:41:00 -0700

    scatter plot with population data

---
+++ scatterPlot.js
@@ -0,0 +1,82 @@
+const bar = d3.select('.bar-chart');
+
+const margin = { left: 150, top: 60, right: 20, bottom: 70 };
+
+const barWidth = +bar.attr('width') - margin.left - margin.right;
+const barHeight = +bar.attr('height') - margin.top - margin.bottom;
+
+
+const render = data => {
+    const xValue = d => d.population;
+    const yValue = d => d.country;
+
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data, xValue)])
+        .range([0, barWidth])
+        .nice();
+
+    const y = d3.scalePoint()
+        .domain(data.map(yValue))
+        .range([0, barHeight])
+        .padding(.5);
+
+    const xAxisTicksFormat = num =>
+        d3.format('.3s')(num)
+            .replace('G', 'B')
+
+    const yAxis = d3.axisLeft(y)
+        .tickSize(-barWidth);
+
+    const xAxis = d3.axisBottom(x)
+        .tickFormat(xAxisTicksFormat)
+        .tickSize(-barHeight);
+
+    const gBar = bar.append('g')
+        .attr('transform', `translate(${margin.left} ${margin.top})`)
+
+    gBar.append('g')
+        .call(yAxis)
+        .selectAll('.domain')
+        .remove();
+
+    const xAxisG = gBar.append('g')
+        .call(xAxis)
+        .attr('transform', `translate(0, ${barHeight})`)
+
+    xAxisG
+        .select('.domain')
+        .remove();
+
+
+
+    gBar
+        .selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('cy', d => y(yValue(d)))
+        .attr('cx', d => x(xValue(d)))
+        .attr('r', d => 15)
+        .attr('fill', 'steelblue');
+console.log(y.bandwidth() / 2)
+    gBar
+        .append('text')
+        .attr('class', 'title')
+        .attr('y', -10)
+        .text('Top 10 Populous Countries')
+
+
+    xAxisG.append('text')
+        .attr('class', 'xaxis-label')
+        .attr('y', 50)
+        .attr('x', barWidth / 2)
+        .attr('fill', 'black')
+        .text('Population')
+}
+
+
+d3.csv('populationData.csv').then(data => {
+    data.forEach(d => d.population = +d.population * 1000)
+    render(data)
+})
+
