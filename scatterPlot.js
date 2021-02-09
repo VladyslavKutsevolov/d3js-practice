5865da90bef45a5647770b171fddd6dfc30f3082
Vladyslav <kytsevolov@gmail.com>
2021-02-08 18:24:38 -0700

    scatter plot with car data

--- scatterPlot.js
+++ scatterPlot.js
@@ -7,37 +7,53 @@ const barHeight = +bar.attr('height') - margin.top - margin.bottom;
 
 
 const render = data => {
-    const xValue = d => d.population;
-    const yValue = d => d.country;
+
+    const title = 'Cars: Horsepower vs. Weight';
+
+    const xValue = d => d.horsepower;
+    const xAxisLabel = 'Horsepower';
+
+    const yValue = d => d.acceleration;
+    const circleRadius = 10;
+    const yAxisLabel = 'Acceleration';
+
 
     const x = d3.scaleLinear()
-        .domain([0, d3.max(data, xValue)])
+        .domain(d3.extent(data, xValue))
         .range([0, barWidth])
         .nice();
 
-    const y = d3.scalePoint()
-        .domain(data.map(yValue))
-        .range([0, barHeight])
-        .padding(.5);
-
-    const xAxisTicksFormat = num =>
-        d3.format('.3s')(num)
-            .replace('G', 'B')
+    const y = d3.scaleLinear()
+        .domain(d3.extent(data, yValue))
+        .range([barHeight, 0])
+        .nice();
 
     const yAxis = d3.axisLeft(y)
-        .tickSize(-barWidth);
+        .tickSize(-barWidth)
+        .tickPadding(15);
 
     const xAxis = d3.axisBottom(x)
-        .tickFormat(xAxisTicksFormat)
-        .tickSize(-barHeight);
+        .tickSize(-barHeight)
+        .tickPadding(10);
 
     const gBar = bar.append('g')
         .attr('transform', `translate(${margin.left} ${margin.top})`)
 
-    gBar.append('g')
+  const yAxisG = gBar.append('g')
         .call(yAxis)
+
+      yAxisG
         .selectAll('.domain')
         .remove();
+
+    yAxisG.append('text')
+        .attr('class', 'yaxis-label')
+        .attr('y', -55)
+        .attr('x', -barHeight / 2)
+        .attr('fill', 'black')
+        .attr('transform', `rotate(-90)`)
+        .attr('text-anchor', 'middle')
+        .text(yAxisLabel);
 
     const xAxisG = gBar.append('g')
         .call(xAxis)
@@ -46,6 +62,13 @@ const render = data => {
     xAxisG
         .select('.domain')
         .remove();
+
+    xAxisG.append('text')
+        .attr('class', 'xaxis-label')
+        .attr('y', 55)
+        .attr('x', barWidth / 2)
+        .attr('fill', 'black')
+        .text(xAxisLabel)
 
 
 
@@ -56,27 +79,28 @@ const render = data => {
         .append('circle')
         .attr('cy', d => y(yValue(d)))
         .attr('cx', d => x(xValue(d)))
-        .attr('r', d => 15)
-        .attr('fill', 'steelblue');
-console.log(y.bandwidth() / 2)
+        .attr('r', d => circleRadius)
+        .attr('fill', 'steelblue')
+        .attr('opacity', '.5');
+
     gBar
         .append('text')
         .attr('class', 'title')
         .attr('y', -10)
-        .text('Top 10 Populous Countries')
-
-
-    xAxisG.append('text')
-        .attr('class', 'xaxis-label')
-        .attr('y', 50)
-        .attr('x', barWidth / 2)
-        .attr('fill', 'black')
-        .text('Population')
+        .text(title)
 }
 
 
-d3.csv('populationData.csv').then(data => {
-    data.forEach(d => d.population = +d.population * 1000)
-    render(data)
-})
-
+d3.csv('https://vizhub.com/curran/datasets/auto-mpg.csv')
+    .then(data => {
+        data.forEach(d => {
+            d.mpg = +d.mpg;
+            d.cylinders = +d.cylinders;
+            d.displacement = +d.displacement;
+            d.horsepower = +d.horsepower;
+            d.weight = +d.weight;
+            d.acceleration = +d.acceleration;
+            d.year = +d.year;
+        });
+        render(data);
+    });
