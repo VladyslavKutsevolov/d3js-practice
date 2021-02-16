9bff75ad48ff88ada7b55fac55035ad7fe555f23
Vladyslav <kytsevolov@gmail.com>
2021-02-16 16:43:43 -0700

    added hover and zoom

--- wordmap.js
+++ wordmap.js
@@ -1,21 +1,39 @@
 
 const svg = d3.select('svg');
 
-const projection = d3.geoOrthographic();
+const projection = d3.geoNaturalEarth1();
 const pathGenerator = d3.geoPath().projection(projection);
 
-svg.append('path')
+const g = svg.append('g')
+
+g.append('path')
     .attr('class', 'sphere')
-    .attr('d', pathGenerator({type: 'Sphere'}))
+    .attr('d', pathGenerator({type: 'Sphere'}));
 
-d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
-    .then(data => {
-      const countries =  topojson.feature(data, data.objects.countries);
 
-      svg.selectAll('path')
-          .data(countries.features)
-          .enter()
-          .append('path')
-          .attr('class', 'country')
-          .attr('d', pathGenerator);
-    })
+
+svg.call(d3.zoom().on('zoom', (e) => {
+    g.attr("transform", (transform = e.transform));
+}) )
+
+
+Promise.all([
+    d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv'),
+    d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+]).then(([tsvData, topoJSONData]) => {
+    const countryName = tsvData.reduce((acc,d) => {
+        acc[d.iso_n3] = d.name
+        return acc;
+    }, {})
+
+    const countries =  topojson.feature(topoJSONData, topoJSONData.objects.countries);
+
+    g.selectAll('path')
+        .data(countries.features)
+        .enter()
+        .append('path')
+        .attr('class', 'country')
+        .attr('d', pathGenerator)
+        .append('title')
+        .text(d => countryName[d.id])
+});
