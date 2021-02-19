a2c54132096ea61af7811f12760bba1115f23c11
Vladyslav <kytsevolov@gmail.com>
2021-02-19 10:43:26 -0700

    setting color on countries depends on development

--- wordmap.js
+++ wordmap.js
@@ -21,19 +21,30 @@ Promise.all([
     d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
     d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
 ]).then(([tsvData, topoJSONData]) => {
-    const countryName = tsvData.reduce((acc,d) => {
-        acc[d.iso_n3] = d.name
+    const rowById = tsvData.reduce((acc,d) => {
+        acc[d.iso_n3] = d
         return acc;
     }, {})
 
     const countries =  topojson.feature(topoJSONData, topoJSONData.objects.countries);
+    const colorScale = d3.scaleOrdinal();
+    const colorValue = d => d.properties.economy;
+
+    countries.features.forEach(d => {
+        Object.assign(d.properties, rowById[d.id])
+    });
+
+    colorScale
+        .domain(countries.features.map(colorValue))
+        .domain(colorScale.domain().sort().reverse())
+        .range(d3.schemeSpectral[colorScale.domain().length]);
 
     g.selectAll('path')
-        .data(countries.features)
-        .enter()
+        .data(countries.features).enter()
         .append('path')
-        .attr('class', 'country')
-        .attr('d', pathGenerator)
+            .attr('class', 'country')
+            .attr('d', pathGenerator)
+            .attr('fill', d => colorScale(colorValue(d)))
         .append('title')
-        .text(d => countryName[d.id])
+            .text(d => d.properties.name + ': '+ colorValue(d))
 });
